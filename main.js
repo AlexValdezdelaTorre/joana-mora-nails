f4785eca52e8dba0f5be61f9b7a01ea623cb97f3
AlexValdezdelaTorre <alexvaldezmor@gmail.com>
2025-07-18 13:37:37 -0600

    Actualizacion de CSS

--- main.js
+++ main.js
@@ -20,3 +20,58 @@
       modal.style.display = "none";
     }
   }
+
+  /* function mostrarGaleria(categoria) {
+    const galerias = document.querySelectorAll('.imagenes-galeria');
+    galerias.forEach(g => g.classList.add('oculto'));
+
+    const galeriaSeleccionada = document.getElementById('galeria-' + categoria);
+    galeriaSeleccionada.classList.remove('oculto');
+  } */
+
+    function abrirGaleria(categoria) {
+    const modal = document.getElementById("modalGaleria");
+    modal.style.display = "block";
+
+    // Oculta todas las galerías
+    const galerias = document.querySelectorAll(".imagenes-galeria");
+    galerias.forEach(g => g.classList.add("oculto"));
+
+    // Muestra la seleccionada
+    const galeriaSeleccionada = document.getElementById("galeria-" + categoria);
+    galeriaSeleccionada.classList.remove("oculto");
+  }
+
+  function cerrarGaleria() {
+    document.getElementById("modalGaleria").style.display = "none";
+  }
+
+  // Cierra si hace clic fuera del modal
+  window.onclick = function(event) {
+    const modal = document.getElementById("modalGaleria");
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  // Abrir imagen en lightbox
+function abrirLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightboxImg.src = src;
+  lightbox.classList.remove("oculto");
+}
+
+// Cerrar lightbox
+function cerrarLightbox() {
+  document.getElementById("lightbox").classList.add("oculto");
+}
+
+// Cerrar al hacer clic fuera de la imagen
+window.addEventListener("click", function (e) {
+  const lightbox = document.getElementById("lightbox");
+  const img = document.getElementById("lightbox-img");
+  if (e.target === lightbox && e.target !== img) {
+    cerrarLightbox();
+  }
+});
